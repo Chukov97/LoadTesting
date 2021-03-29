93f7a1e54ab826cfbb63b5f87cfcdd2ca6f22ebb
Илья Чуков <ilyachuk@gmail.com>
2021-03-30 01:16:14 +0300

    Параметризация и корреляция в LoadRunner

---
+++ test2_param_corr/Action.c
@@ -0,0 +1,165 @@
+Action()
+{
+	int p;
+	
+	lr_start_transaction("Bilet");
+
+/*Correlation comment - Do not change!  Original value='130987.086107736zHDHztipctVzzzzHtAtcfpiiQtf' Name ='userSession' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=userSession",
+		"TagName=input",
+		"Extract=value",
+		"Name=userSession",
+		"Type=hidden",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		"RequestUrl=*/nav.pl*",
+		LAST);
+
+	web_url("webtours", 
+		"URL={Host}/webtours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_save_string("Stroka", "Param");
+
+	/* login */
+
+	//lr_think_time(19);
+
+	web_submit_data("login.pl",
+		"Action={Host}/cgi-bin/login.pl",
+		"Method=POST",
+		"TargetFrame=body",
+		"RecContentType=text/html",
+		"Referer={Host}/cgi-bin/nav.pl?in=home",
+		"Snapshot=t2.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=userSession", "Value={userSession}", ENDITEM,
+		"Name=username", "Value=jojo", ENDITEM,
+		"Name=password", "Value=bean", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		"Name=login.x", "Value=54", ENDITEM,
+		"Name=login.y", "Value=10", ENDITEM,
+		LAST);
+	/* flig */
+
+	//lr_think_time(11);
+
+	web_url("Search Flights Button", 
+		"URL={Host}/cgi-bin/welcome.pl?page=search", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer={Host}/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+	
+	web_reg_save_param_ex(
+    "ParamName=OFlight", 
+    "LB/IC=name=\"outboundFlight\" value=\"",
+    "RB/IC=\"",
+    "Ordinal=1",
+	LAST);
+
+
+	web_submit_data("reservations.pl", 
+		"Action={Host}/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer={Host}/cgi-bin/reservations.pl?page=welcome", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=depart", "Value={City}", ENDITEM, 
+		"Name=departDate", "Value={Data1}", ENDITEM, 
+		"Name=arrive", "Value={City2}", ENDITEM, 
+		"Name=returnDate", "Value={Data2}", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=seatPref", "Value=None", ENDITEM, 
+		"Name=seatType", "Value=Coach", ENDITEM, 
+		"Name=.cgifields", "Value=roundtrip", ENDITEM, 
+		"Name=.cgifields", "Value=seatType", ENDITEM, 
+		"Name=.cgifields", "Value=seatPref", ENDITEM, 
+		"Name=findFlights.x", "Value=63", ENDITEM, 
+		"Name=findFlights.y", "Value=10", ENDITEM, 
+		LAST);
+
+	web_submit_data("reservations.pl_2", 
+		"Action={Host}/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer={Host}/cgi-bin/reservations.pl", 
+		"Snapshot=t5.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=outboundFlight", "Value={OFlight}", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=seatType", "Value=Coach", ENDITEM, 
+		"Name=seatPref", "Value=None", ENDITEM, 
+		"Name=reserveFlights.x", "Value=51", ENDITEM, 
+		"Name=reserveFlights.y", "Value=11", ENDITEM, 
+		LAST);
+
+	web_submit_data("reservations.pl_3", 
+		"Action={Host}/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer={Host}/cgi-bin/reservations.pl", 
+		"Snapshot=t6.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=firstName", "Value=Jojo", ENDITEM, 
+		"Name=lastName", "Value=Bean", ENDITEM, 
+		"Name=address1", "Value=", ENDITEM, 
+		"Name=address2", "Value=", ENDITEM, 
+		"Name=pass1", "Value=Jojo Bean", ENDITEM, 
+		"Name=creditCard", "Value=", ENDITEM, 
+		"Name=expDate", "Value=", ENDITEM, 
+		"Name=oldCCOption", "Value=", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=seatType", "Value=Coach", ENDITEM, 
+		"Name=seatPref", "Value=None", ENDITEM, 
+		"Name=outboundFlight", "Value={OFlight}", ENDITEM, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=returnFlight", "Value=", ENDITEM, 
+		"Name=JSFormSubmit", "Value=off", ENDITEM, 
+		"Name=.cgifields", "Value=saveCC", ENDITEM, 
+		"Name=buyFlights.x", "Value=58", ENDITEM, 
+		"Name=buyFlights.y", "Value=17", ENDITEM, 
+		LAST);
+
+	//lr_think_time(21);
+
+	web_url("Itinerary Button", 
+		"URL={Host}/cgi-bin/welcome.pl?page=itinerary", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer={Host}/cgi-bin/nav.pl?page=menu&in=flights", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		LAST);
+	
+	p=10;
+	
+	lr_end_transaction("Bilet", LR_AUTO);
+	lr_output_message("Comment");
+	lr_output_message(lr_eval_string("{City}"));
+	lr_output_message("%d",p);
+	lr_output_message(lr_eval_string("{Param}"));
+	
+	return 0;
+}
